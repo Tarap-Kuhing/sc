b1a63ee9a256efe2457c49d10a9dd3010e974400
Tarap-Kuhing <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-06-17 18:35:22 +0800

    @Baung

--- xray/ins-xray.sh
+++ xray/ins-xray.sh
@@ -491,35 +491,6 @@ systemctl restart nginx
 systemctl enable runn
 systemctl restart runn
 
-cd /usr/bin/
-# vmess
-#wget -O uuid "https://raw.githubusercontent.com/Tarap-Kuhing/sc/main/xray/uuid.sh" && chmod +x uuid
-#wget -O trialvmess "https://raw.githubusercontent.com/Tarap-Kuhing/sc/main/xray/trialvmess.sh" && chmod +x trialvmess
-#wget -O renew-vmess "https://raw.githubusercontent.com/Tarap-Kuhing/sc/main/xray/renew-vmess.sh" && chmod +x renew-vmess
-#wget -O del-vmess "https://raw.githubusercontent.com/Tarap-Kuhing/sc/main/xray/del-vmess.sh" && chmod +x del-vmess
-#wget -O cek-vmess "https://raw.githubusercontent.com/Tarap-Kuhing/sc/main/xray/cek-vmess.sh" && chmod +x cek-vmess
-
-# vless
-#wget -O add-vless "https://raw.githubusercontent.com/Tarap-Kuhing/sc/main/xray/add-vless.sh" && chmod +x add-vless
-#wget -O trialvless "https://raw.githubusercontent.com/Tarap-Kuhing/sc/main/xray/trialvless.sh" && chmod +x trialvless
-#wget -O renew-vless "https://raw.githubusercontent.com/Tarap-Kuhing/sc/main/xray/renew-vless.sh" && chmod +x renew-vless
-#wget -O del-vless "https://raw.githubusercontent.com/Tarap-Kuhing/sc/main/xray/del-vless.sh" && chmod +x del-vless
-#wget -O cek-vless "https://raw.githubusercontent.com/Tarap-Kuhing/sc/main/xray/cek-vless.sh" && chmod +x cek-vless
-
-# trojan
-#wget -O add-tr "https://raw.githubusercontent.com/Tarap-Kuhing/sc/main/xray/add-tr.sh" && chmod +x add-tr
-#wget -O trialtrojan "https://raw.githubusercontent.com/Tarap-Kuhing/sc/main/xray/trialtrojan.sh" && chmod +x trialtrojan
-#wget -O del-tr "https://raw.githubusercontent.com/Tarap-Kuhing/sc/main/xray/del-tr.sh" && chmod +x del-tr
-#wget -O renew-tr "https://raw.githubusercontent.com/Tarap-Kuhing/sc/main/xray/renew-tr.sh" && chmod +x renew-tr
-#wget -O cek-tr "https://raw.githubusercontent.com/Tarap-Kuhing/sc/main/xray/cek-tr.sh" && chmod +x cek-tr
-
-# shadowsocks
-#wget -O add-ssws "https://raw.githubusercontent.com/Tarap-Kuhing/sc/main/xray/add-ssws.sh" && chmod +x add-ssws
-#wget -O trialssws "https://raw.githubusercontent.com/Tarap-Kuhing/sc/main/xray/trialssws.sh" && chmod +x trialssws
-#wget -O del-ssws "https://raw.githubusercontent.com/Tarap-Kuhing/sc/main/xray/del-ssws.sh" && chmod +x del-ssws
-#wget -O renew-ssws "https://raw.githubusercontent.com/Tarap-Kuhing/sc/main/xray/renew-ssws.sh" && chmod +x renew-ssws
-
-
 sleep 0.5
 yellow() { echo -e "\\033[33;1m${*}\\033[0m"; }
 yellow "xray/Vmess"
